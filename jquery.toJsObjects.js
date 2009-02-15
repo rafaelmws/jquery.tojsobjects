007f784146276a21bcefedc23d133639b3a53d82
Rafael <rafael.mws@gmail.com>
2009-02-15 19:51:16 -0300

    add file

---
+++ jquery.toJsObjects.js
@@ -0,0 +1,26 @@
+/*
+ * jQuery toJsObjects 0.1
+ *
+ * Copyright (c) 2009 
+ * AUTHOR : Rafael Martins <rafael.mws@gmail.com>
+ * Dual licensed under the MIT and GPL  licenses.
+ *
+ */
+
+$.fn.toObjects = function(properties){
+	try	{
+		objects = [];
+		this.each(function(index){
+			var obj = {};
+			for(i=0; i < properties.length; i ++){
+				obj[properties[i]] = $(this).attr(properties[i]);
+			}
+			obj.index = index;
+			objects.push(objeto);
+		});
+		return objects	
+	}
+	catch(exception){
+		console.log(exception);
+	}
+};
